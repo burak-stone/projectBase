cd0b395c3e2b431792a2504bc81fe2d5b9dfa82b
burak-stone <muhammed_fb_burak@hotmail.com>
2024-10-19 18:02:46 +0300

    #6 User Auth Yapısı Kuruldu

--- db/models/Users.js
+++ db/models/Users.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const Enum = require('../../config/Enum');
+const CustomError = require('../../lib/Error');
+const bcrypt = require("bcryptjs")
 
 const schema = mongoose.Schema({
     email : {type : String, required: true, unique: true} ,
@@ -17,6 +20,20 @@ const schema = mongoose.Schema({
 
 
 class Users extends mongoose.Model {
+
+    validPassword(password){
+        return bcrypt.compareSync(password, this.password)
+    }
+
+
+
+    static validateFieldsBeforeAuth(email , password){
+        if(typeof password !== "string" || password.length < Enum.PASS_LENGTH){
+            throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error", "email or password wrong")
+        }else{
+            return null
+        }
+    }
 
 }
 
