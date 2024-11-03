cdd230d2f5b34fe2b2ea1ddf61b7e7da37b537fd
burak-stone <muhammed_fb_burak@hotmail.com>
2024-11-03 22:45:16 +0200

    bugs fixed

--- routes/categories.js
+++ routes/categories.js
@@ -12,6 +12,22 @@ const i18n = new (require("../lib/i18n"))(config.DEFAULT_LANG);
 const emitter = require("../lib/Emitter")
 const excelExport = new (require("../lib/Export"))();
 const fs = require("fs")
+const Import = new (require("../lib/Import"))();
+const multer = require("multer");
+const path = require('path');
+
+
+let multerStorage = multer.diskStorage({
+    destination: (req, file, next) => {
+        next(null, config.FILE_UPLOAD_PATH)
+    },
+    filename: (req, file, next) => {
+        next(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+    }
+})
+
+const upload = multer({ storage: multerStorage }).single("pb_file");
+
 
 router.all("*", auth.authenticate(), (req, res, next)=>{
     next()
@@ -122,6 +138,39 @@ router.post("/export", auth.checkRoles("category_export") , async(req,res) => {
         let errorResponse = Response.errorResponse(error)
 
         res.status(errorResponse.code).json(Response.errorResponse(error))
+    }
+})
+
+router.post("/import", auth.checkRoles("category_add"), upload, async (req, res) => {
+    try {
+
+        let file = req.file;
+
+        let rows = Import.fromExcel(file.path);
+
+        for (let i = 1; i < rows.length; i++) {
+            // eslint-disable-next-line no-unused-vars
+            let [name, is_active, user, created_at, updated_at] = rows[i];
+            if (name) {
+                let createdCategories = await Categories.create({
+                    name,
+                    is_active,
+                    created_by: req.user._id
+                });
+
+                AuditLogs.info(req.user.email, "Categories", "Add", createdCategories)
+                logger.info(req.user.email, "Categories", "Add", createdCategories)
+                emitter.getEmitter("notifications").emit("messages", {message: createdCategories.name+ " is added"})
+            }
+
+        }
+
+        res.status(Enum.HTTP_CODES.CREATED).json(Response.successResponse(req.body, Enum.HTTP_CODES.CREATED));
+
+    } catch (error) {
+        logger.error(req.user.email, "Categories", "Add", error)
+        let errorResponse = Response.errorResponse(error);
+        res.status(errorResponse.code).json(Response.errorResponse(error));
     }
 })
 
